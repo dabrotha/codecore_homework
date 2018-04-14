8cc983fbf3bc753d06648f30899f45dc73d304b9
Alex Kulyk <alexanderkulyk@gmail.com>
2018-04-13 19:12:56 -0700

    added drawTopBorder function

--- week01/boxit.js
+++ week01/boxit.js
@@ -10,8 +10,38 @@ let outputStr = "";
 function drawLine (num) {
     let output = '';
   for (let i = 0; i<num; i++)
-    {output += "-";}
+    {output += "-"};
     return output;
 }
 
-console.log(drawLine(input));
+
+
+function drawTopBorder (num) {
+let output="┏";
+for (let i=1; i<(num-1); i++) {
+    output+="-";
+}
+output += "┓";
+return output;
+};
+
+console.log(drawTopBorder(input));
+
+
+
+
+
+
+// drawTopBorder, drawMiddleBorder and drawBottomBorder Functions
+// Write three functions: drawTopBorder, drawMiddleBorder and drawBottomBorder. Each function should take a number, return a line of length including corner pieces. You can make use of drawLine to implement these functions.
+
+// Example usage:
+
+// drawTopBorder(4) // returns '┏━━━━┓'
+// drawTopBorder(0) // returns '┏┓'
+
+// drawMiddleBorder(8) // returns '┣━━━━━━━━━┫'
+// drawMiddleBorder(0) // returns '┣┫'
+
+// drawBottomBorder(2) // returns '┗━━┛'
+// You will need special characters to "draw" the box. You can find them listed in this Wikipedia article. To use them, you can copy & paste them where needed.
