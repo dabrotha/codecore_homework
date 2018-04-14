5689f6a093af1e75f4abf7a896d345c6eda745c4
Alex Kulyk <alexanderkulyk@gmail.com>
2018-04-13 18:14:44 -0700

    added drawLine function

--- week01/boxit.js
+++ week01/boxit.js
@@ -1,5 +1,17 @@
 
+const args = process.argv.slice(2);
+let input = args;
+
+let outputStr = "";
 
 
 
 
+function drawLine (num) {
+    let output = '';
+  for (let i = 0; i<num; i++)
+    {output += "-";}
+    return output;
+}
+
+console.log(drawLine(input));
