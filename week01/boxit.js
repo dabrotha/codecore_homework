ec863b94bcd63c336d42075cce94b3c207b3c53b
Alex Kulyk <alexanderkulyk@gmail.com>
2018-04-15 22:27:54 -0700

    modified drawMiddleBorder function so that it works depending on whether parameter is number or string

--- week01/boxit.js
+++ week01/boxit.js
@@ -23,8 +23,13 @@ return output;
 
 function drawMiddleBorder(num){
     let output="┣";
+      if (typeof(num)==="number"){
+          for (let i=0; i<num; i++) {
+        output+="━";}// end of loop
+
+      } else {
       for (let i=1; i<(num.length+1); i++) {
-        output+="━";
+        output+="━";} // end of loop
       }
     output += "┫";
     return output;
@@ -45,30 +50,44 @@ function drawBarsAround (str) {
     output += "┃";
     return output;
     };    
+    
+function longestValLength (arr) { //will return .length of the longest value in array
+    let retVal = 0;
+  for (i=0; i<arr.length; i++) {
+      if (retVal < arr[i].length) {
+          console.log(arr[i].length);
+          retVal = arr[i].length;
+      } else {
+
+      }
+    return retVal;
+  }
+}
 
 function boxIt(str) {
     let output = "";
+    let longestKey = longestValLength(str); //this variable whil be used for border lengths
     if (str.length === 1) { // case if there is only one value in array
-        output += (drawTopBorder(str[0].length) + "\n" + drawBarsAround(str)) + "\n" + drawBottomBorder(str[0].length);
+        output += (drawTopBorder(longestKey) + "\n" + drawBarsAround(str)) + "\n" + drawBottomBorder(str[0].length);
 
     } else if (str.length === 0) { // case if there is no input
         output += ('┏┓\n┗┛');
 
-    } else if (str.length > 1) {
-        output += (drawTopBorder(str[0].length) + "\n");
+    } else if (str.length > 1) { // case when there is more than one value in array
+        output += (drawTopBorder(longestKey) + "\n");
         for (i=0; i<str.length; i++) {
 
             output += (drawBarsAround(str[i]) + "\n"); 
             
             if (i<(str.length-1)) {
-                output += (drawMiddleBorder(str[i]) + "\n");
+                output += (drawMiddleBorder(longestKey) + "\n");
             } else {}
         
         }
-        output += (drawBottomBorder(str[0].length));
+        output += (drawBottomBorder(longestKey));
 
     } else {
-        console.log("There is an error")}; // this shouldn't ever happen
+        console.log("There is an error")}; // this means that input.length < 0 and shouldn't ever happen
 
     output += "\n";
     return output;
